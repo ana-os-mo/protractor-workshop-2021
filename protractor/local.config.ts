50c57d88f7866ed04d5ce9332b9e970755765579
ana-os-mo <88041760+ana-os-mo@users.noreply.github.com>
2021-10-28 04:56:08 -0500

    css-selector time modification (#6)

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -3,8 +3,10 @@ import { reporter } from './helpers/reporter';
 
 export const config: Config = {
   framework: 'jasmine',
-  specs: [ '../test/google.spec.js' ],
+  specs: [ '../test/buy-tshirt.spec.js' ],
   SELENIUM_PROMISE_MANAGER: false,
+  getPageTimeout: 10000,
+
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     reporter();
