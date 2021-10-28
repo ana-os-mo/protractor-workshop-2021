a4fdfc61597c239e7402ee33ef5becb38dc19d0e
ana-os-mo <anmosoriomo@unal.edu.co>
2021-10-27 22:17:39 -0500

    promise manager deactivation and chrome headless

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -5,6 +5,14 @@ export const config: Config = {
   framework: 'jasmine',
   specs: [ '../test/google.spec.js' ],
   SELENIUM_PROMISE_MANAGER: false,
+
+  capabilities: {
+    browserName: 'chrome',
+    chromeOptions: {
+      args: ['--headless', '--disable-gpu']
+    }
+  },
+
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     reporter();
